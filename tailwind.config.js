ecc163dcf19f5a6f20f44c702377f014687666cd
Rachidmlk <rascid.seoul@gmail.com>
2024-08-28 12:52:34 +0100

    final v

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ["Edu VIC WA NT Beginner"],
+        slogan: ["Signika Negative"],
+        span: ["Qwitcher Grypen"]
+      }
+        
+      
+    },
+  },
+  plugins: [],
+}
+
